c221ce4fbab3856a6c8fd7ee0f87f8dce421da62
ggmaleva <ggmaleva@yandex.ru>
2022-01-24 15:24:50 +0300

    fix import

--- src/main/java/dev/vality/fraudo/payment/factory/FraudVisitorFactory.java
+++ src/main/java/dev/vality/fraudo/payment/factory/FraudVisitorFactory.java
@@ -1,8 +1,5 @@
 package dev.vality.fraudo.payment.factory;
 
-import dev.vality.fraudo.dto.AggregatorDto;
-import dev.vality.fraudo.dto.FinderDto;
-import dev.vality.fraudo.dto.ResolverDto;
 import dev.vality.fraudo.dto.VisitorDto;
 import dev.vality.fraudo.model.BaseModel;
 import dev.vality.fraudo.payment.visitor.impl.FirstFindVisitorImpl;
