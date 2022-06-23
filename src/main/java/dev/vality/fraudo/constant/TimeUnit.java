19757d4a351c5ded23a6995a8762360b194f1fca
ggmaleva <ggmaleva@yandex.ru>
2022-06-23 14:54:50 +0300

    fix time unit

--- src/main/java/dev/vality/fraudo/constant/TimeUnit.java
+++ src/main/java/dev/vality/fraudo/constant/TimeUnit.java
@@ -6,6 +6,7 @@ import lombok.experimental.UtilityClass;
 public class TimeUnit {
 
     public static final String MINUTES = "minutes";
+    public static final String HOURS = "hours";
     public static final String DAYS = "days";
     public static final String CALENDAR_MONTHS = "calendar_months";
 
--- src/main/java/dev/vality/fraudo/model/TimeWindow.java
+++ src/main/java/dev/vality/fraudo/model/TimeWindow.java
@@ -3,14 +3,12 @@ package dev.vality.fraudo.model;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.temporal.ChronoUnit;
-
 @Data
 @Builder
 public class TimeWindow {
 
     private int start;
     private int end;
-    private ChronoUnit timeUnit;
+    private String timeUnit;
 
 }
